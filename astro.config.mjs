7ae65023a24e5651bc500ce99ddf1c371f95c751
HannaLiman <limanhanna@gmail.com>
2026-06-17 22:02:30 +0300

    setup for hosting on github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://hannaliman.github.io',
+	base: '/portfolio',
 	integrations: [tailwind()],
 });
